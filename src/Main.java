5b6f73662dcaf7fb2e4f608514408b5d93fdea61
Кристина <duleba@eml.ru>
2022-11-05 18:28:57 +0700

    initiall commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args) {
 
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println();
 
+        int clientOS1 = 0;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println();
+
+        int years = 2015;
+        if (years % 4 == 0 && years % 100 != 0 && years % 400 == 0) {
+            System.out.println( years + " год является високосным");
+
+        } else {
+            System.out.println(years + " год не является високосным");
+        }
     }
 }
+
